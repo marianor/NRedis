871b0bb5e6781b2c8e3d18d07198702c219dd8f7
Mariano Rodriguez <1547454+marianor@users.noreply.github.com>
2019-05-13 13:36:32 -0400

    SSL replaced by TLS

---
+++ Redis/Transport/TlsTcpTransport.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace NRedis.Transport
+{
+    public class TlsTcpTransport : TcpTransport
+    {
+        public TlsTcpTransport(string host, int port) : base(host, port)
+        {
+        }
+
+        protected override Stream GetStream(TcpClient client)
+        {
+            var stream = new SslStream(base.GetStream(client), false);
+            stream.AuthenticateAsClient(Host);
+            return stream;
+        }
+
+        protected override async Task<Stream> GetStreamAsync(TcpClient client)
+        {
+            var stream = new SslStream(base.GetStream(client), false);
+            await stream.AuthenticateAsClientAsync(Host).ConfigureAwait(false);
+            return stream;
+        }
+    }
+}
